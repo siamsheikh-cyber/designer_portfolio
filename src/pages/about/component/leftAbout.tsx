c7205a5f78fe93e2a54528f2d0d3ee3ca995ad1d
Siam Sheikh <siamthecoder@gmail.com>
2025-12-12 07:45:18 +0600

    text-justify

--- src/pages/about/component/leftAbout.tsx
+++ src/pages/about/component/leftAbout.tsx
@@ -1,10 +1,10 @@
 function LeftAbout() {
     return (
         <div className="basis-[50%]">
-            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] mb-3.5">I am a professional Graphic Designer and Video Editor with over 2 years of experience in creating impactful visuals and polished video content. I combine creativity with technical precision to bring ideas to life.</p>
-            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] mb-3.5">I am skilled in:
+            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] mb-3.5 text-justify">I am a professional Graphic Designer and Video Editor with over 2 years of experience in creating impactful visuals and polished video content. I combine creativity with technical precision to bring ideas to life.</p>
+            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] mb-3.5 text-justify">I am skilled in:
                 Adobe Illustrator, Adobe Photoshop, Adobe Premiere Pro, After Effects, Filmora, Microsoft Word & Microsoft Excel.</p>
-            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] ">
+            <p className="text-gray-500 dark:text-gray-50 text-[16px] leading-[26px] text-justify">
                 I believe great design is powerful—it tells a story, solves problems, and creates lasting impressions. Every project I work on is crafted with purpose, clarity, and a strong visual identity.
             </p>
         </div>
